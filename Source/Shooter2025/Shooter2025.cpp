5493a4dfa8a512a337dfb33ecab57d4df60414d1
SammiiiB <sambrooks.pdx@gmail.com>
2025-07-30 20:24:57 -0700

    idk

---
+++ Source/Shooter2025/Shooter2025.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Shooter2025.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Shooter2025, "Shooter2025" );
---
+++ Source/Shooter2025/cPlusPlusGenVisualStudioFile.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "cPlusPlusGenVisualStudioFile.h"
+
+// Sets default values
+AcPlusPlusGenVisualStudioFile::AcPlusPlusGenVisualStudioFile()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AcPlusPlusGenVisualStudioFile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AcPlusPlusGenVisualStudioFile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AcPlusPlusGenVisualStudioFile::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
